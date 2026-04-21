84af34e8bfa1a93fc993f1201cbdc5cc67ad7510
os1378 <os1378@srmist.edu.in>
2026-04-21 10:51:14 +0530

    added UC2

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,4 @@
-public class TicTacToe {
+/*public class TicTacToe {
     public static void main(String[] args) {
         char[][] board = new char[3][3];
 
@@ -17,5 +17,36 @@ public class TicTacToe {
             }
             System.out.println();
         }
+    }
+}
+*/
+public class TicTacToe {
+    public static void main(String[] args) {
+
+        // Using fully qualified class name instead of import
+        java.util.Random rand = new java.util.Random();
+
+        int toss = rand.nextInt(2);
+
+        String player1 = "Player 1";
+        String player2 = "Player 2";
+
+        char symbol1, symbol2;
+        String currentPlayer;
+
+        if (toss == 0) {
+            currentPlayer = player1;
+            symbol1 = 'X';
+            symbol2 = 'O';
+        } else {
+            currentPlayer = player2;
+            symbol2 = 'X';
+            symbol1 = 'O';
+        }
+
+        System.out.println("Toss Result:");
+        System.out.println(currentPlayer + " starts first!");
+        System.out.println(player1 + " symbol: " + symbol1);
+        System.out.println(player2 + " symbol: " + symbol2);
     }
 }
